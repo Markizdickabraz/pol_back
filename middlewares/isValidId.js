9c914dccdbbdaa0f03a789bf9418e9d779c55425
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2024-03-31 09:45:58 +0300

    fix isValidId

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -2,9 +2,10 @@ const { isValidObjectId } = require("mongoose");
 const { HttpError } = require('../helpers');
 
 const isValidId = (req, res, next) => {
-    const { id } = req.params;
-    if (!isValidObjectId(id)) {
-        next(HttpError(400, `${id} is not a valid id`));
+    const { contactId } = req.params;
+    console.log(req.params);
+    if (!isValidObjectId(contactId)) {
+        next(HttpError(400, `${contactId} is not a valid id`));
     } else {
         next();
     }
